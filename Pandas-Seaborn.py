54073a88c580496696c648562324a24722f85d01
kartik arora <arorakartik923@gmail.com>
2021-03-06 11:28:32 +0530

    examples with binary queries and alignment

--- Pandas-Seaborn.py
+++ Pandas-Seaborn.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 
 
+print("Example 1")
+
 list = [1,2,3,4,5,6,7,8,9,10]
 
 ser =pd.Series(list)
@@ -17,7 +19,8 @@ print(ser.head())
 print(ser.head(5))
 
 # Working of iloc and loc in indexing
-name = [1,2,3,4,5,6]
+
+print("Example 2")
 
 data = pd.DataFrame({
 						'Country':['IND','AU','USA','CAN'],
@@ -33,3 +36,29 @@ print(data.loc[	['Person1','Person4'],['Country']])
 # Here in iloc we have to use integer indexes instead of string or char
 print(data.iloc[[0,3],[0]])
 
+## Boolean queries
+
+print("Example 3")
+var1 = pd.Series(['a','b','c','d','e'])
+var2 = pd.Series([False,True,True,False,True])
+
+print(var1[var2])
+
+#
+print("Example 3.1 querying")
+
+#seaborn comes with a default data of titanic passengers
+
+titanic = sns.load_dataset('titanic')
+
+# print Passesngers with gender male, age<35 and pclass 2
+print(titanic[(titanic.sex=='male')&(titanic.age<35)&(titanic.pclass==2)])
+
+# Alignment : Label are important not the order.
+
+a1 = pd.Series([200,300,400,np.nan], index=['a','b','c','d'])
+a2= pd.Series([40,150,310,100], index=['a','g','c','d'])
+
+print(a1.add(a2))
+
+print(a1.align(a2))
